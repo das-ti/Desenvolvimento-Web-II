0a034d522491aecb7a8bf3e5c2fe06b4514318b2
das.ti@outlook.com.br <das.ti@outlook.com.br>
2025-08-27 07:49:37 -0300

    final update files index.php and style.css

--- Aula03-Atividade01/index.php
+++ Aula03-Atividade01/index.php
@@ -12,7 +12,7 @@
     <main>
         <section>
             <h1>TopPizzaria</h1>
-            <h3>Novo Pedido</h3>
+            <h2>Novo Pedido</h2>
             <form action="#" method="post">
                 <label for="txtnome">Seu nome:</label><br>
                 <input type="text" name="txtnome" id="txtnome" required>
@@ -29,79 +29,92 @@
                 <input type="radio" value="Não" name="rdborda" id="rdnao" checked>Não
                 <input type="radio" value="Sim" name="rdborda" id="rdsim">Sim
                 <br><br>
-                <label for="">Bebidas (você pode escolher mais de uma):</label><br>
-                <input type="checkbox" value="8" name="ckbebidas[]">Refri de lata - R$ 8,00 <br>
-                <input type="checkbox" value="10" name="ckbebidas[]">Suco 500ml - R$ 10,00 <br>
-                <input type="checkbox" value="5" name="ckbebidas[]">Água 500ml - R$ 5,00 <br>
+                <label for="ckbebidas">Bebidas (você pode escolher mais de uma):</label><br>
+                <input type="checkbox" value="8" name="ckbebidas[]" id="ckbebidas">Refri de lata - R$ 8,00 <br>
+                <input type="checkbox" value="10" name="ckbebidas[]" id="ckbebidas">Suco 500ml - R$ 10,00 <br>
+                <input type="checkbox" value="5" name="ckbebidas[]" id="ckbebidas">Água 500ml - R$ 5,00 <br>
                 <input type="submit" value="Finalizar Pedido" name="btnpedido">
             </form>
         </section>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nome = $_POST["txtnome"];
+            $sabor = $_POST["sltsabor"];
+            $borda = $_POST["rdborda"];
+
+            $precoPizza = 0;
+
+            if ($sabor == "Mussarela") {
+                $precoPizza += 30;
+            } else if ($sabor == "Calabresa") {
+                $precoPizza += 32;
+            } else if ($sabor == "Caipira") {
+                $precoPizza += 35;
+            } else {
+                $precoPizza += 37;
+            }
+
+            $precoBorda = 5;
+            if ($borda == "Sim") {
+                $precoPizza += $precoBorda;
+            } else {
+                $precoBorda = 0;
+                $precoPizza += $precoBorda;
+            }
+            echo
+            "<section id='resumo-pedido' style='display: block'>
+                        <p id='mensagem-pedido'><strong>" . $nome . ", </strong><b>aqui está o resumo do seu pedido: </b></p>
+                        <ul id='lista-pedido'>
+                        <li><b>Pizza sabor:</b> " . $sabor . " - R$ " . $precoPizza . ",00</li>
+                        <li><b>Borda recheada:</b> " . $borda . " - R$ " . $precoBorda . ",00</li>
+                        <li><b>Bebidas:</b> </li>
+                        ";
+
+            $precoBebida = 0;
+
+            if (isset($_POST['ckbebidas'])) {
+
+                $bebidas = $_POST["ckbebidas"];
+                $tipoBebida = "";
+
+                foreach ($bebidas as $bebida) {
+
+                    if ($bebida == 8) {
+                        $precoBebida += $bebida;
+                        $tipoBebida = "Refri de lata - R$ 8,00";
+                    } else if ($bebida == 10) {
+                        $precoBebida += $bebida;
+                        $tipoBebida = "Suco 500ml - R$ 10,00";
+                    } else if ($bebida == 5) {
+                        $precoBebida += $bebida;
+                        $tipoBebida = "Água 500ml - R$ 5,00";
+                    }
+                    echo
+                    "<ul id='lista-bebidas'>
+                        <li>" . $tipoBebida . "</li>
+                    </ul>";
+                }
+            } else {
+                // O checkbox não foi marcado!
+                echo
+                    "<ul id='lista-bebidas'>
+                            <li>Sem bebidas R$ " . $precoBebida . ",00</li>
+                    </ul>
+                </ul>";
+            }
+            $total = 0;
+
+            $total = $precoPizza + $precoBebida;
+
+            if (isset($_POST['btnpedido'])) {
+                echo "<p id='total-pedido'><b>Total a pagar: </b>R$" . ($total) . ",00</p>";
+            }
+
+            echo "<br><br><a href='index.php'>Fazer novo pedido</a>";
+
+            "</section>";
+        }
+        ?>
     </main>
 
 </html>
-
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nome = $_POST["txtnome"];
-    $sabor = $_POST["sltsabor"];
-    $borda = $_POST["rdborda"];
-    $bebidas = $_POST["ckbebidas"];
-
-    $precoPizza = 0;
-
-    if ($sabor == "Mussarela") {
-        $precoPizza += 30;
-    } else if ($sabor == "Calabresa") {
-        $precoPizza += 32;
-    } else if ($sabor == "Caipira") {
-        $precoPizza += 35;
-    } else {
-        $precoPizza += 37;
-    }
-
-    if ($borda == "Sim") {
-        $precoPizza += 5;
-    } 
-    // else {
-    //     $preco;
-    // }
-
-    echo "<h4>Resumo do Pedido</h4>";
-    echo "<br>Nome: " . $nome;
-    echo "<br>Sabor: " . $sabor;
-    echo "<br>Borda: " . $borda;
-    echo "<br>Bebidas: ";
-
-    $precoBebida = 0;
-    $tipoBebida = "";
-
-    foreach ($bebidas as $bebida) {
-
-        if ($bebida == 8) {
-            $precoBebida += $bebida;
-            $tipoBebida = "Refri";
-        } else if ($bebida == 10) {
-            $precoBebida += $bebida;
-            $tipoBebida = "Suco";
-        } else if ($bebida == 5) {
-            $precoBebida += $bebida;
-            $tipoBebida = "Água";
-        } else {
-            $precoBebida = 0;
-            $tipoBebida = " ";
-        }
-        echo
-        "<ul>
-                <li>" . $tipoBebida . " - valor: R$ " . $precoBebida . ",00</li>
-            </ul>";
-    }
-
-    $total = 0;
-
-    $total = $precoPizza + $precoBebida;
-
-    if (isset($_POST['btnpedido'])) {
-        echo "<br>Total a pagar: R$" . ($total);
-    }
-}
-?>
