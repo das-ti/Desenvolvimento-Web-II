a294f2cc859aad3d85ea0b3e77b9606a69c30986
das.ti@outlook.com.br <das.ti@outlook.com.br>
2025-09-09 15:43:21 -0300

    add files project aula03

--- Aula03-Atividade01/index.php
+++ Aula03-Atividade01/index.php
@@ -10,38 +10,39 @@
 
 <body>
     <main>
-        <section>
-            <h1>TopPizzaria</h1>
-            <h2>Novo Pedido</h2>
-            <form action="#" method="post">
-                <label for="txtnome">Seu nome:</label><br>
-                <input type="text" name="txtnome" id="txtnome" required>
-                <br><br>
-                <label for="sabor">Escolha o sabor da pizza:</label><br>
-                <select name="sltsabor" id="sabores">
-                    <option value="Mussarela">Mussarela - R$ 30,00</option>
-                    <option value="Calabresa">Calabresa - R$ 32,00</option>
-                    <option value="Caipira">Caipira - R$ 35,00</option>
-                    <option value="Brocolis">Brócolis - R$ 37,00</option>
-                </select>
-                <br><br>
-                <label for="rdsim">Borda recheada:</label><br>
-                <input type="radio" value="Não" name="rdborda" id="rdnao" checked>Não
-                <input type="radio" value="Sim" name="rdborda" id="rdsim">Sim
-                <br><br>
-                <label for="ckbebidas">Bebidas (você pode escolher mais de uma):</label><br>
-                <input type="checkbox" value="8" name="ckbebidas[]" id="ckbebidas">Refri de lata - R$ 8,00 <br>
-                <input type="checkbox" value="10" name="ckbebidas[]" id="ckbebidas">Suco 500ml - R$ 10,00 <br>
-                <input type="checkbox" value="5" name="ckbebidas[]" id="ckbebidas">Água 500ml - R$ 5,00 <br>
-                <input type="submit" value="Finalizar Pedido" name="btnpedido">
-            </form>
-        </section>
-        <?php
+            <section id="novo-pedido">
+                <h1>TopPizzaria</h1>
+                <h2>Novo Pedido</h2>
+                <form action="#" method="post">
+                    <label for="txtnome">Seu nome:</label><br>
+                    <input type="text" name="txtnome" id="txtnome" required>
+                    <br><br>
+                    <label for="sabor">Escolha o sabor da pizza:</label><br>
+                    <select name="sltsabor" id="sabores">
+                        <option value="Mussarela">Mussarela - R$ 30,00</option>
+                        <option value="Calabresa">Calabresa - R$ 32,00</option>
+                        <option value="Caipira">Caipira - R$ 35,00</option>
+                        <option value="Brocolis">Brócolis - R$ 37,00</option>
+                    </select>
+                    <br><br>
+                    <label for="rdsim">Borda recheada:</label><br>
+                    <input type="radio" value="Não" name="rdborda" id="rdnao" checked>Não
+                    <input type="radio" value="Sim" name="rdborda" id="rdsim">Sim
+                    <br><br>
+                    <label for="ckbebidas">Bebidas (você pode escolher mais de uma):</label><br>
+                    <input type="checkbox" value="8" name="ckbebidas[]" id="ckbebidas">Refri de lata - R$ 8,00 <br>
+                    <input type="checkbox" value="10" name="ckbebidas[]" id="ckbebidas">Suco 500ml - R$ 10,00 <br>
+                    <input type="checkbox" value="5" name="ckbebidas[]" id="ckbebidas">Água 500ml - R$ 5,00 <br>
+                    <input type="submit" value="Finalizar Pedido" name="btnpedido">
+                </form>
+            </section>
+            <?php
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
             $nome = $_POST["txtnome"];
             $sabor = $_POST["sltsabor"];
             $borda = $_POST["rdborda"];
-
+            
             $precoPizza = 0;
 
             if ($sabor == "Mussarela") {
@@ -53,7 +54,7 @@
             } else {
                 $precoPizza += 37;
             }
-
+            
             $precoBorda = 5;
             if ($borda == "Sim") {
                 $precoPizza += $precoBorda;
@@ -63,7 +64,7 @@
             }
             echo
             "<section id='resumo-pedido' style='display: block'>
-                        <p id='mensagem-pedido'><strong>" . $nome . ", </strong><b>aqui está o resumo do seu pedido: </b></p>
+            <p id='mensagem-pedido'><strong>" . $nome . ", </strong><b>aqui está o resumo do seu pedido: </b></p>
                         <ul id='lista-pedido'>
                         <li><b>Pizza sabor:</b> " . $sabor . " - R$ " . $precoPizza . ",00</li>
                         <li><b>Borda recheada:</b> " . $borda . " - R$ " . $precoBorda . ",00</li>
@@ -95,9 +96,8 @@
                     </ul>";
                 }
             } else {
-                // O checkbox não foi marcado!
                 echo
-                    "<ul id='lista-bebidas'>
+                "<ul id='lista-bebidas'>
                             <li>Sem bebidas R$ " . $precoBebida . ",00</li>
                     </ul>
                 </ul>";
@@ -112,7 +112,8 @@
 
             echo "<br><br><a href='index.php'>Fazer novo pedido</a>";
 
-            "</section>";
+            "</section>
+            <?php endif;?>";
         }
         ?>
     </main>
